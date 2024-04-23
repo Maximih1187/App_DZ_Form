ae39e90ca96dd0834b0cf050522816ba71eeeaa6
Maximka <benko_0287.mail.ru>
2024-04-23 21:29:51 +0300

    first_commit

---
+++ src/components/heroesAddForm/HeroesAddForm.js
@@ -0,0 +1,99 @@
+
+
+// Задача для этого компонента:
+// Реализовать создание нового героя с введенными данными. Он должен попадать
+// в общее состояние и отображаться в списке + фильтроваться
+// Уникальный идентификатор персонажа можно сгенерировать через uiid
+// Усложненная задача:
+// Персонаж создается и в файле json при помощи метода POST
+// Дополнительно:
+// Элементы <option></option> желательно сформировать на базе
+// данных из фильтров'
+
+//import { useDispatch } from "react-redux";
+//import { heroesFetched } from '../../actions';
+import { useCallback, useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
+
+
+const HeroesAddForm = () => {
+    const [name, setName] = useState('')
+    const [text, setText] = useState('')
+    const [element, setElement] = useState('')
+    // const dispatch = useDispatch()
+    const { v4: uuidv4 } = require('uuid');
+
+
+
+
+    const hendelSubmit = () => {
+        const res = {
+            id: uuidv4(),
+            name: name,
+            description: text,
+            element: element,
+        }
+        console.log(res);
+        fetch("http://localhost:3001/heroes", {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(res)
+        })
+
+    }
+
+    return (
+        <form onSubmit={hendelSubmit} className="border p-4 shadow-lg rounded">
+            <div className="mb-3">
+                <label htmlFor="name" className="form-label fs-4">Имя нового героя</label>
+                <input
+                    required
+                    onChange={(e) => setName(e.target.value)}
+                    type="text"
+                    name="name"
+                    className="form-control"
+                    value={name}
+                    id="name"
+                    placeholder="Как меня зовут?" />
+            </div>
+
+            <div className="mb-3">
+                <label htmlFor="text" className="form-label fs-4">Описание</label>
+                <input
+                    required
+                    onChange={(e) => setText(e.target.value)}
+                    name="text"
+                    className="form-control"
+                    id="text"
+                    placeholder="Что я умею?"
+                    value={text}
+                    style={{ "height": '130px' }} />
+            </div>
+
+            <div className="mb-3">
+                <label htmlFor="element" className="form-label">Выбрать элемент героя</label>
+                <select
+                    required
+                    onChange={(e) => setElement(e.target.value)}
+                    className="form-select"
+                    id="element"
+                    value={element}
+                    name="element">
+
+                    <option >Я владею элементом...</option>
+                    <option value="fire">Огонь</option>
+                    <option value="water">Вода</option>
+                    <option value="wind">Ветер</option>
+                    <option value="earth">Земля</option>
+                </select>
+            </div>
+
+            <button type="submit" className="btn btn-primary">Создать</button>
+        </form>
+    )
+
+}
+export default HeroesAddForm;
---
+++ src/components/heroesListItem/HeroesListItem.js
@@ -0,0 +1,56 @@
+
+
+
+
+const HeroesListItem = ({ id, name, description, element, setStateDelete }) => {
+
+    let elementClassName;
+
+    switch (element) {
+        case 'fire':
+            elementClassName = 'bg-danger bg-gradient';
+            break;
+        case 'water':
+            elementClassName = 'bg-primary bg-gradient';
+            break;
+        case 'wind':
+            elementClassName = 'bg-success bg-gradient';
+            break;
+        case 'earth':
+            elementClassName = 'bg-secondary bg-gradient';
+            break;
+        default:
+            elementClassName = 'bg-warning bg-gradient';
+    }
+
+    const closeHero = (e) => {
+
+        const deleteHero = e.target.id
+        console.log(deleteHero);
+        fetch(`http://localhost:3001/heroes/${deleteHero}`, { method: 'DELETE' })
+            .then(setStateDelete(false))
+
+    }
+
+
+
+    return (
+        <li key={id}
+            className={`card flex-row mb-4 shadow-lg text-white ${elementClassName}`}>
+            <img src="https://avatars.mds.yandex.net/i?id=6d55156a048d6d3663ec379823ed3e410d06df87-12421823-images-thumbs&n=13"
+                className="img-fluid w-25 d-inline"
+                alt="unknown hero"
+                style={{ 'objectFit': 'cover', "scale": "1.1" }} />
+            <div className="card-body">
+
+                <h3 className="card-title">{name}</h3>
+                <p className="card-text">{description}</p>
+            </div>
+            <span className="position-absolute top-0 start-100 translate-middle badge border rounded-pill bg-light">
+                <button onClick={closeHero} id={id} type="button" className="btn-close btn-close" aria-label="Close"></button>
+            </span>
+        </li>
+    )
+}
+
+export default HeroesListItem;
