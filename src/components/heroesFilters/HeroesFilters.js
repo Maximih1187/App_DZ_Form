d02be22ed78e417507545ce11292565213707b38
Maximka <benko_0287.mail.ru>
2024-04-26 15:08:48 +0300

    Финал. Без проверки!

--- src/components/heroesFilters/HeroesFilters.js
+++ src/components/heroesFilters/HeroesFilters.js
@@ -11,41 +11,33 @@ import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { heroesFilter } from '../../actions';
 
-
 const HeroesFilters = () => {
 
     const { request } = useHttp();
-    const [stateButtons, setstateButtons] = useState([])
+    const [stateButtons, setStateButtons] = useState([])
     const { heroes } = useSelector(state => state)
-    const [category, setCategory] = useState("")
+    const [category, setCategory] = useState("all")
     const dispath = useDispatch()
-
 
     useEffect(() => {
         request("http://localhost:3001/filters")
-            .then(data => setstateButtons(data));
-
+            .then(data => setStateButtons(data))
         renderFilter(heroes)
     }, [category]);
 
     const activeButtonCategory = (e) => {
-        setCategory(e.target.id)
+        setCategory(e.target.id);
+
     }
-
-
-
 
     const filterHeroesCategory =
         heroes.filter((item) => {
             return item.element === category
         })
 
-
     const renderFilter = () => {
         dispath(heroesFilter(filterHeroesCategory))
     }
-
-
 
     return (
         <div className="card shadow-lg mt-4">
@@ -53,7 +45,7 @@ const HeroesFilters = () => {
                 <p className="card-text">Отфильтруйте героев по элементам</p>
                 <div className="btn-group">
                     {stateButtons.map((item) => {
-                        return <button onClick={activeButtonCategory} id={item.key} key={item.key} className={item.className}>{item.name}</button>
+                        return <button disabled={category === item.key} onClick={activeButtonCategory} id={item.key} key={item.key} className={item.className}>{item.name}</button>
                     })}
                 </div>
             </div>
